d04ce1befced45a41cee43b967efefd143442b8b
Trevor Lemon <duhlemman@gmail.com>
2019-04-27 14:41:52 -0700

    Published Babel plugin and brought it in

--- babel.config.js
+++ babel.config.js
@@ -1,47 +1,5 @@
-const { parse, relative } = require('path');
 const { version } = require('./package.json');
 const { aliases } = require('./conf.app');
-
-const a2rp = () => {
-  return {
-    visitor: {
-      StringLiteral(path, state) {
-        const { aliases } = state.opts;
-        const { filename } = state.file.opts;
-        
-        if( !aliases || !filename ) return;
-        
-        const aliasKeys = Object.keys(aliases).map((alias) => alias);
-        const aliasRegEx = new RegExp(`^(${ aliasKeys.join('|') })`);
-        const currString = path.node.value;
-        const match = currString.match(aliasRegEx);
-        
-        if( match ){
-          const alias = match[1];
-          const transformed = currString.replace(alias, aliases[alias]);
-          
-          let relativePath = relative(
-            parse(filename).dir,
-            transformed
-          );
-        
-          if(!relativePath.startsWith('.')) relativePath = `./${ relativePath }`;
-          
-          // console.log(
-          //   '---\n',
-          //   `file: ${ filename }\n`,
-          //   `requested: ${ transformed }\n`,
-          //   `relative: ${ relativePath }\n`,
-          // );
-        
-          path.node.value = relativePath;
-        }
-        
-        return;
-      },
-    },
-  };
-};
 
 module.exports = (api) => {
   api.cache(true);
@@ -50,7 +8,7 @@ module.exports = (api) => {
   // https://babeljs.io/docs/en/6.26.3/babel-preset-env
   return {
     plugins: [
-      [a2rp, { aliases }],
+      ['@noxx/babel-plugin-a2rp', { aliases }],
       ['transform-define', {
         'global.appVersion': `v${ version }`,
       }],
