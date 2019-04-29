ebef847b23ca1cc591acc96c775806d07bfafcd3
tlemon <trevor.lemon@nike.com>
2019-04-29 14:28:58 -0700

    Refactored Icon
    - It now exports `Icon` and `IconLink`.
      - `IconLink` will allow for `use` cases where svg's are inlined.
      - `Icon` will download the svg from the cdn and componetize it.
    - Added a CORs rule for static asset requests so they can be requested 
    from the Client.

--- src/modules/@org/org-react-icon/index.js
+++ src/modules/@org/org-react-icon/index.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
+import reactParser from 'html-react-parser';
 import { string } from 'prop-types';
+import { ICONS_URL } from '@ORG/org-icons';
 import styles from './styles';
 
-const Icon = ({
+const basePropTypes = {
+  className: string,
+  desc: string,
+  name: string,
+  title: string,
+};
+const baseDefaultProps = {
+  className: '',
+};
+
+class Icon extends Component {
+  static getManifest(url) {
+    return fetch(url).then((resp) => resp.json());
+  }
+  
+  static getSVG(url) {
+    return fetch(url).then((resp) => resp.text());
+  }
+  
+  constructor() {
+    super();
+    
+    this.state = {
+      svg: undefined,
+    };
+  }
+  
+  componentDidMount() {
+    const { name } = this.props;
+    
+    Icon.getManifest(`${ ICONS_URL }/manifest.json`)
+      .then((manifest) => Icon.getSVG(manifest[name]))
+      .then((svg) => {
+        this.setState({ svg });
+      });
+  }
+  
+  render() {
+    const { className, desc, name, title } = this.props;
+    const { svg } = this.state;
+    
+    if(!svg) return null;
+    
+    const titleId = (title) ? `title__${ name }` : '';
+    const descId = (desc) ? `desc__${ name }` : '';
+    let cl = `
+      class="${ styles } org-icon ${ className }"
+      role="img"
+    `;
+    
+    if(titleId || descId) cl += `aria-labelledby="${ titleId } ${ descId }"`;
+    
+    const titleNode = (title) ? `<title id="${ titleId }">${ title }</title>` : '';
+    const descNode = (desc) ? `<desc id="${ descId }">${ desc }</desc>` : '';
+    const _svg = svg.replace(/<svg(\b[^>]*)>/, `<svg ${ cl }$1>${ titleNode }${descNode}`);
+    
+    return reactParser(_svg);
+  }
+}
+
+Icon.propTypes = {
+  ...basePropTypes,
+};
+Icon.defaultProps = {
+  ...baseDefaultProps,
+};
+
+const IconLink = ({
   className,
   desc,
   name,
@@ -10,13 +79,15 @@ const Icon = ({
 }) => {
   const titleId = (title) ? `title__${ name }` : '';
   const descId = (desc) ? `desc__${ name }` : '';
+  const svgProps = {
+    className: `${ styles } org-icon ${ className }`,
+    role: 'img',
+  };
+  
+  if(titleId || descId) svgProps['aria-labelledby'] = `${ titleId } ${ descId }`;
   
   return (
-    <svg
-      className={`${ styles } org-icon ${ className }`}
-      role="img"
-      aria-labelledby={`${titleId} ${descId}`}
-    >
+    <svg {...svgProps}>
       {title && <title id={titleId}>{title}</title>}
       {desc && <desc id={descId}>{desc}</desc>}
       <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={`#org-icon_${ name }`}></use>
@@ -24,14 +95,14 @@ const Icon = ({
   );
 };
 
-Icon.propTypes = {
-  className: string,
-  desc: string,
-  name: string,
-  title: string,
+IconLink.propTypes = {
+  ...basePropTypes,
 };
-Icon.defaultProps = {
-  className: '',
+IconLink.defaultProps = {
+  ...baseDefaultProps,
 };
 
-export default Icon;
+export {
+  Icon,
+  IconLink,
+};
