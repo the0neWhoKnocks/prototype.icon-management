7b4fa6cd91d97ea4459f8695265d4362616cc35d
Trevor Lemon <duhlemman@gmail.com>
2019-05-02 07:55:12 -0700

    Added in a search input for icons

--- src/server/views/icons/client.js
+++ src/server/views/icons/client.js
@@ -40,6 +40,134 @@ const tab = ({ content, title }) => `
   </tab>
 `;
 
+const renderIconCards = (query) => {
+  const cards = Object.keys(window.manifest).map((icon) => {
+    if(icon === '_version') return '';
+    
+    if(query && !icon.includes(query)) return '';
+    
+    const src = window.manifest[icon];
+    const relSrc = `/${ src.split('/').slice(3).join('/') }`;
+    
+    return `
+      <icon-card>
+        <card>
+          <icon-wrapper>
+            <icon><img src="${ relSrc }" /></icon>
+            <icon-text>${ icon }</icon-text>
+          </icon-wrapper>
+          <button
+            name="usage"
+            data-name="${ icon }"
+            data-src="${ relSrc }"
+          >Usage</button>
+        </card>
+      </icon-card>
+    `;
+  }).join('');
+  
+  els.iconsCards.innerHTML = cards;
+};
+
+const handleUsageClick = (data) => {
+  const { name, src } = data;
+  const tabs = [
+    tab({
+      title: 'img',
+      content:
+        '<p>Standard img request format.</p>'
+        + `<code class="html">&lt;img src="${ window.location.origin }${ src }" /&gt;</code>`
+      ,
+    }),
+    tab({
+      title: 'svg',
+      content:
+        '<p>This approach allows for customization of the icon since it adds a copy of the full SVG to the DOM.</p>'
+        + '<code class="js">'
+        + "import { Icon } from '@ORG/org-react-icon';"
+        + '\n'  
+        + '\nconst App = () => ('
+        + '\n  &lt;div&gt;'
+        + '\n    &lt;Icon' 
+        + `\n      name="${ name }"`
+        + '\n      title="Icon title"'
+        + '\n      desc="An icon description"'
+        + '\n    /&gt;'
+        + '\n  &lt;/div&gt;'
+        + '\n);'
+        + '\n</code>'
+      ,
+    }),
+    tab({
+      title: 'svg link',
+      content:
+        '<p>This approach has the same benefits of the svg usage, but also trims down Client requests.</p>'
+        + '<h4>Server</h4>'
+        + '<ul>'
+        + '  <li>Downloads and caches the icons on Server startup.</li>'
+        + '  <li>Passes icons to Shell</li>'
+        + '</ul>'
+        + '<code class="js">'
+        + "import { getIcons, iconsToSymbols } from '@ORG/org-icons';"
+        + '\n\n'
+        + '\n// Fetches and caches the specified icons'
+        + '\nconst iconsPromise = getIcons({'
+        + "\n  icons: ['cake', 'mood', 'mood_bad'],"
+        + "\n  version: 'v1.1.0',"
+        + '\n});'
+        + '\n\n'
+        + '\n// Once the icons have been fetched, pass them to your View'
+        + '\niconsPromise.then((icons) => {'
+        + '\n  renderShell({ icons });'
+        + '\n});'
+        + '</code>'
+        + '<h4>Shell</h4>'
+        + '<ul>'
+        + '  <li>Inlines the SVGs for link reference usage.</li>'
+        + '</ul>'
+        + '<code class="js">'
+        + 'const shell = ({ icons }) => \`'
+        + '\n  &lt;html&gt;'
+        + '\n    &lt;body&gt;'
+        + '\n      ${ icons }'
+        + '\n    &lt;/body&gt;'
+        + '\n  &lt;/html&gt;'
+        + '\n\`;'
+        + '</code>'
+        + '<h4>Client</h4>'
+        + '<ul>'
+        + '  <li>Uses a reference to one of the inlined SVGs</li>'
+        + '</ul>'
+        + '<code class="js">'
+        + "import { IconLink } from '@ORG/org-react-icon';"
+        + '\n'  
+        + '\nconst App = () => ('
+        + '\n  &lt;div&gt;'
+        + '\n    &lt;IconLink' 
+        + `\n      name="${ name }"`
+        + '\n      title="Icon title"'
+        + '\n      desc="An icon description"'
+        + '\n    /&gt;'
+        + '\n  &lt;/div&gt;'
+        + '\n);'
+        + '\n</code>'
+      ,
+    }),
+  ];
+  els.panelContent.innerHTML = `
+    <tabs>
+      <tabs-row>${ tabs.join('') }</tabs-row>
+      <tabs-content></tabs-content>
+    </tabs>
+  `;
+  wireUpTabs();
+  els.panel.classList.toggle('open');
+};
+
+const handleSearch = (ev) => {
+  renderIconCards(ev.target.value);
+};
+
 const addListeners = () => {
   document.body.addEventListener('click', (ev) => {
     const el = ev.target;
@@ -47,98 +175,7 @@ const addListeners = () => {
     if(el.nodeName === 'BUTTON') {
       switch(el.getAttribute('name')){
         case 'usage': {
-          const { name, src } = el.dataset;
-          const tabs = [
-            tab({
-              title: 'img',
-              content:
-                '<p>Standard img request format.</p>'
-                + `<code class="html">&lt;img src="${ window.location.origin }${ src }" /&gt;</code>`
-              ,
-            }),
-            tab({
-              title: 'svg',
-              content:
-                '<p>This approach allows for customization of the icon since it adds a copy of the full SVG to the DOM.</p>'
-                + '<code class="js">'
-                + "import { Icon } from '@ORG/org-react-icon';"
-                + '\n'  
-                + '\nconst App = () => ('
-                + '\n  &lt;div&gt;'
-                + '\n    &lt;Icon' 
-                + `\n      name="${ name }"`
-                + '\n      title="Icon title"'
-                + '\n      desc="An icon description"'
-                + '\n    /&gt;'
-                + '\n  &lt;/div&gt;'
-                + '\n);'
-                + '\n</code>'
-              ,
-            }),
-            tab({
-              title: 'svg link',
-              content:
-                '<p>This approach has the same benefits of the svg usage, but also trims down Client requests.</p>'
-                + '<h4>Server</h4>'
-                + '<ul>'
-                + '  <li>Downloads and caches the icons on Server startup.</li>'
-                + '  <li>Passes icons to Shell</li>'
-                + '</ul>'
-                + '<code class="js">'
-                + "import { getIcons, iconsToSymbols } from '@ORG/org-icons';"
-                + '\n\n'
-                + '\n// Fetches and caches the specified icons'
-                + '\nconst iconsPromise = getIcons({'
-                + "\n  icons: ['cake', 'mood', 'mood_bad'],"
-                + "\n  version: 'v1.1.0',"
-                + '\n});'
-                + '\n\n'
-                + '\n// Once the icons have been fetched, pass them to your View'
-                + '\niconsPromise.then((icons) => {'
-                + '\n  renderShell({ icons });'
-                + '\n});'
-                + '</code>'
-                + '<h4>Shell</h4>'
-                + '<ul>'
-                + '  <li>Inlines the SVGs for link reference usage.</li>'
-                + '</ul>'
-                + '<code class="js">'
-                + 'const shell = ({ icons }) => \`'
-                + '\n  &lt;html&gt;'
-                + '\n    &lt;body&gt;'
-                + '\n      ${ icons }'
-                + '\n    &lt;/body&gt;'
-                + '\n  &lt;/html&gt;'
-                + '\n\`;'
-                + '</code>'
-                + '<h4>Client</h4>'
-                + '<ul>'
-                + '  <li>Uses a reference to one of the inlined SVGs</li>'
-                + '</ul>'
-                + '<code class="js">'
-                + "import { IconLink } from '@ORG/org-react-icon';"
-                + '\n'  
-                + '\nconst App = () => ('
-                + '\n  &lt;div&gt;'
-                + '\n    &lt;IconLink' 
-                + `\n      name="${ name }"`
-                + '\n      title="Icon title"'
-                + '\n      desc="An icon description"'
-                + '\n    /&gt;'
-                + '\n  &lt;/div&gt;'
-                + '\n);'
-                + '\n</code>'
-              ,
-            }),
-          ];
-          els.panelContent.innerHTML = `
-            <tabs>
-              <tabs-row>${ tabs.join('') }</tabs-row>
-              <tabs-content></tabs-content>
-            </tabs>
-          `;
-          wireUpTabs();
-          els.panel.classList.toggle('open');
+          handleUsageClick(el.dataset);
           break;
         }
 
@@ -149,12 +186,17 @@ const addListeners = () => {
       }
     }
   });
+  
+  els.searchInput.addEventListener('input', handleSearch);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
   els.panel = document.querySelector('panel');
   els.panelContent = document.querySelector('panel-content');
   els.panelMask = document.querySelector('panel-mask');
+  els.searchInput = document.querySelector('[name="search"]');
+  els.iconsCards = document.querySelector('icon-cards');
   
   addListeners();
+  renderIconCards();
 });
